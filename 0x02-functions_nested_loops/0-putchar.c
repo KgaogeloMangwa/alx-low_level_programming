16c14f3792196b85596568753c0f4850559502d4
KgaogeloMangwa <kgaogeloextra@gmail.com>
2023-02-21 05:23:47 -0800

    char

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "putchar.h"
+
+/**
+ * main- entry point of our program
+ *
+ * description - a program that prints _putchar followed by a new line
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char *out = "_putchar";
+
+	while (*out)
+	{
+		_putchar(*out);
+		out++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
