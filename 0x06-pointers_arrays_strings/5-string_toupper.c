25d4923793410124d9bd4faba48987ccab310213
KgaogeloMangwa <kgaogeloextra@gmail.com>
2023-03-01 16:39:55 -0800

    upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - a function that changes all lowercase
+ * of a string to uppercase
+ * @s: string input
+ *
+ * Return: uppercase string
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
+	}
+	return (s);
+}
