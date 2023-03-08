d6e0e68a7eece54f2e560eb60e6f4ceb57da71c0
KgaogeloMangwa <kgaogeloextra@gmail.com>
2023-03-08 03:52:52 -0800

    reversestring

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,4 +2,14 @@
 
 /**
  * _print_rev_recursion - a function that prints a string in reverse
- * */
+ * @s: the string to reverse and print
+ * Return: 0 always
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+		_print_rev_recursion(s + 1);
+	else
+		return;
+	_putchar(*s);
+}
