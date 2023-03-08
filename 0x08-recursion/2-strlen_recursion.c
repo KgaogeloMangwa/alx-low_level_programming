ba6544354bff630793dc5ddb036e4e6a177b947b
KgaogeloMangwa <kgaogeloextra@gmail.com>
2023-03-08 05:32:51 -0800

    givingup

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -5,11 +5,10 @@
  * @s: the string input to return length of
  * Return: always 0
  */
-int _strlen_recursion(char *s);
+int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 		return (0);
-	s++;
-	return (_strlen_recursion(s) + 1);
-
+	else
+		return (1 + _strlen_recursion(s + 1));
 }
