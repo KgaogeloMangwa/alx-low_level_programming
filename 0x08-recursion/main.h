e765978c9d39a67ae273cf608e41bd8fe560c746
KgaogeloMangwa <kgaogeloextra@gmail.com>
2023-03-08 00:23:26 -0800

    stringprint

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,12 @@
+#ifndef PRINCE_M
+#define PRINCE_M
+
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+
+#endif
