a03b0e420b59812d35a16a9f85d34eba06481735
KgaogeloMangwa <kgaogeloextra@gmail.com>
2023-03-20 16:37:48 -0700

    poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef POPPY_D
+#define POPPY_D
+
+/**
+ * struct dog - a function that defines a dog structure
+ * @name: 1st member
+ * @age: 2nd member
+ * @owner: 3rd member
+ */
+typedef struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+} poppy_d;
+
+void init_poppy(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+poppy_d *new_dog(char *name, float age, Char *owner);
+void free_poppy(poppy_d *d);
+
+#endif
