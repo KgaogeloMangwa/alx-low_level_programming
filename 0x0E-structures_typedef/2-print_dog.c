ff95680ab36a1adbe853818fd16fca70abdc3f81
KgaogeloMangwa <kgaogeloextra@gmail.com>
2023-03-20 17:27:32 -0700

    printdog1

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,7 +11,7 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 		if (d->name)
-			printf("Name: s\n", d->name);
+			printf("Name: %s\n", d->name);
 		else
 			printf("Name: (nil)\n");
 		printf("Age: %f\n", d->age);
