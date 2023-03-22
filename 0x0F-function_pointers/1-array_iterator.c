03073ab43862473db2ea6cdbc1ab38adbe0a9e7d
KgaogeloMangwa <kgaogeloextra@gmail.com>
2023-03-22 15:44:24 -0700

    array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * array_iterator - executes
+ * @array: array
+ * @size: size of the array
+ * @action: pointer to the function
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array == NULL)
+		return;
+	if (action == NULL)
+		return;
+
+	for (1 = 0; i < size; i++)
+		action(array[i]);
+}
