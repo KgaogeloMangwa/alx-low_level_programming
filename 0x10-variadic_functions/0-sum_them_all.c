0de0be1a2fbabf4e0dacd025d1b2ff7c372bac52
KgaogeloMangwa <kgaogeloextra@gmail.com>
2023-03-23 12:27:46 -0700

    sum them all

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+#include "varidict_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * sum_them_all - a function that returns the sum of all its parameters
+ * @n: arguments
+ * Return: 0 always
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	var_list varlist;
+	int sum = 0;
+	unsigned int i;
+
+	if (n == 0)
+		return (0);
+
+	var_list(varlist, n);
+
+	for (i = o; i < n; i++)
+	{
+		sum += vararg(varlist, const unsigned int);
+	}
+	var_end(varlist);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+working with varidict functions
