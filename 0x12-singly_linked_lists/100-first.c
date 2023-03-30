9426ced127aa8f1b1b7612ec144d86b0a63c050c
KgaogeloMangwa <kgaogeloextra@gmail.com>
2023-03-30 13:23:39 -0700

    printfirst

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+/**
+ * print_before_main - a function that prints a specified message
+ * before main
+ */
+
+void print_before_main(void)
+{
+	printf("you're beat! and yet, you must allow, \n");
+	printf("I bore my house upon my back!\n");
+}
